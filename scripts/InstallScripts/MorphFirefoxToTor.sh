9b267f50fa4c682c289614023a6290453e0a41ea
gdallasdye <gdallasdye@gmail.com>
2020-09-29 19:56:05 -0400

    Fix, trim script

--- scripts/InstallScripts/MorphFirefoxToTor.sh
+++ scripts/InstallScripts/MorphFirefoxToTor.sh
@@ -11,28 +11,22 @@ rm -rf ~/.mozilla
 sudo rm -f /usr/lib/firefox-esr/defaults/pref/prawn-settings.js
 sudo rm -f /usr/lib/firefox-esr/prawn.cfg
 
-#Next, we remove then purge firefox-esr then install firefox-esr and tor
+#Next, we remove then purge firefox-esr then reinstall firefox-esr and tor
 sudo apt remove -y firefox-esr
 sudo apt purge -y firefox-esr
-sudo apt install -y firefox-esr tor
-
-#Next, the user with UID 1000 is probably the only user. Let's add them to the tor group
-#TODO: may not be necessary?
-#TODO: test the line below
-#sudo usermod -a -G tor uid=1000
-#newgrp tor
+sudo apt install -y firefox-esr 
 
 #We should try installing curl, incase it's not already
 sudo apt install -y curl
 
-#We probably want to start tor before we follow the next steps
-sudo systemctl enable --now tor
+#Save installing tor for last
+sudo apt install -y tor 
 
-#Then we start to reuse Jeremy Rand's documentation.
-#TODO: make a test, and make improvements. 
+#Torsocks should be usable. Then we start to reuse Jeremy Rand's documentation.
+#TODO: Torsocks may not be immediately useable. Investigate whether we should wait some seconds, consider resume support incase the user (needs to?) reboots.
 
 #(substitute the tag for whatever Tor Browser release you want to use)
-curl --output 000-tor-browser-0.js http://jqs44zhtxl2uo6gk.onion/tor-browser.git/plain/browser/app/profile/000-tor-browser.js?h=tor-browser-78.3.0esr-10.0-2-build2 
+torsocks -i  curl --output 000-tor-browser-0.js http://jqs44zhtxl2uo6gk.onion/tor-browser.git/plain/browser/app/profile/000-tor-browser.js?h=tor-browser-78.3.0esr-10.0-2-build2 
 
 grep -v "^# " 000-tor-browser-0.js | grep -v "^#expand" > 000-tor-browser-1.js
 
